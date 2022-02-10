2503af4940883446e428385475bc1ecc281ffeff
taicout6 <taicout6@gmail.com>
2022-02-10 17:21:14 -0300

    Estrutura básica

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (_req, res) => {
+  res.send(`<h1>Escutando na porta ${PORT}</h1>`)
+});
+
+app.listen(PORT, () => {
+  console.log(`Online na porta ${PORT}`);
+});
