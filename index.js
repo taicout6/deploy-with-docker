33da9610b8c9a4127d27c71df4db429de6a15cc7
taicout6 <taicout6@gmail.com>
2022-02-10 18:17:08 -0300

    add eslint

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (_req, res) => {
-  res.send(`<h1>Escutando na porta ${PORT}</h1>`)
+  res.send(`<h1>Escutando na porta ${PORT}</h1>`);
 });
 
 app.listen(PORT, () => {
